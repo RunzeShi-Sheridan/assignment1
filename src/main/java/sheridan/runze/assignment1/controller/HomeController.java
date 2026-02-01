025b0a1b6dd5f8c7add0188240e845fe9b61f2b8
RunzeShi <shisun@sheridancollege.ca>
2026-01-31 22:09:41 -0500

    Create initial word.html with form, reset link, and default image

--- src/main/java/sheridan/runze/assignment1/controller/HomeController.java
+++ src/main/java/sheridan/runze/assignment1/controller/HomeController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String root() {
+        return "redirect:/word";
+    }
+    @GetMapping("/word")
+    public String wordPage() {
+        return "word";
     }
 }
